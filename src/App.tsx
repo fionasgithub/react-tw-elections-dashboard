c0b7bcef46051117a54baf7b80b6aaa3765212e0
fiona <fy800328@gmail.com>
2026-03-06 15:01:17 +0800

    feat: rename home to dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "@/pages/Home";
+import DashboardPage from "@/pages/DashboardPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home></Home>} />
+        <Route path="/" element={<DashboardPage></DashboardPage>} />
         <Route path="/county/:countyId" element={<div>County</div>} />
       </Routes>
     </BrowserRouter>
