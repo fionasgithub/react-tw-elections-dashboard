25e5d757d962d30b43a3c22fdf33f15fc37c27ca
fiona <fy800328@gmail.com>
2026-03-07 10:31:05 +0800

    feat(route): add county detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardPage from "@/pages/DashboardPage";
+import CountyDetail from "@/pages/CountyDetail";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<DashboardPage></DashboardPage>} />
-        <Route path="/county/:countyId" element={<div>County</div>} />
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/county/:countyId" element={<CountyDetail />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/CountyDetail.tsx
@@ -0,0 +1,11 @@
+import MainLayout from "@/components/layout/MainLayout";
+
+function CountyDetail() {
+  return (
+    <MainLayout>
+      <div>County</div>
+    </MainLayout>
+  );
+}
+
+export default CountyDetail;
